336556928574dc491642eb409b280c23c24993a7
Ben Matson <bmatson@uccs.edu>
2015-11-09 13:02:48 -0700

    Updated DB

--- Eggzlist_db.sql
+++ Eggzlist_db.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Oct 26, 2015 at 06:57 PM
+-- Generation Time: Nov 09, 2015 at 09:02 PM
 -- Server version: 5.6.26
 -- PHP Version: 5.6.12
 
@@ -36,7 +36,23 @@ CREATE TABLE IF NOT EXISTS `Listings` (
   `finish` datetime DEFAULT NULL,
   `inventory` int(11) NOT NULL,
   `private` tinyint(1) NOT NULL DEFAULT '0'
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `Listings`
+--
+
+INSERT INTO `Listings` (`listID`, `sellerID`, `pickup`, `price`, `status`, `start`, `finish`, `inventory`, `private`) VALUES
+(1, 1, 0, 5, 0, '2015-10-28 00:00:00', NULL, 36, 0),
+(4, 1, 0, 6, 0, '2015-10-28 00:00:00', NULL, 24, 1),
+(5, 4, 1, 6, 1, '2015-11-04 00:00:00', NULL, 12, 0),
+(6, 4, 0, 6, 1, '2015-11-05 00:00:00', NULL, 24, 0),
+(7, 3, 1, 5, 1, '2015-11-06 00:00:00', NULL, 24, 0),
+(8, 3, 1, 5, 1, '2015-11-07 00:00:00', NULL, 36, 0),
+(9, 5, 1, 8, 1, '2015-11-08 00:00:00', NULL, 18, 0),
+(10, 5, 1, 8, 1, '2015-11-09 00:00:00', NULL, 18, 0),
+(11, 2, 0, 6, 1, '2015-11-11 00:00:00', NULL, 12, 0),
+(12, 2, 0, 6, 1, '2015-11-12 00:00:00', NULL, 24, 0);
 
 -- --------------------------------------------------------
 
@@ -64,10 +80,26 @@ CREATE TABLE IF NOT EXISTS `Sellers` (
   `feed` varchar(128) DEFAULT NULL,
   `eggrate` int(11) NOT NULL,
   `breeds` varchar(512) NOT NULL,
-  `addr` varchar(1024) DEFAULT NULL,
+  `street` varchar(512) NOT NULL,
+  `city` varchar(512) NOT NULL,
+  `state` varchar(2) NOT NULL,
+  `pcode` int(5) NOT NULL,
   `xroad` varchar(1024) DEFAULT NULL,
+  `lat` double NOT NULL,
+  `lng` double NOT NULL,
   `rating` int(11) NOT NULL DEFAULT '0'
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `Sellers`
+--
+
+INSERT INTO `Sellers` (`sellerID`, `numChick`, `feed`, `eggrate`, `breeds`, `street`, `city`, `state`, `pcode`, `xroad`, `lat`, `lng`, `rating`) VALUES
+(1, 5, 'pikes beak ', 28, 'Leghorn', '9398 west 104th drive', 'westminster', 'co', 80918, 'hen drive, rooster street', 39.882616, -105.103585, 0),
+(2, 10, 'Organic', 63, 'Plymouth Rock', '3616 sheffield lane', 'colorado springs', 'co', 80907, NULL, 38.884279, -104.798245, 0),
+(3, 5, 'Organic', 28, 'Long horn', '5061 perry street', 'denver', 'co', 80221, NULL, 39.788498, -105.039475, 0),
+(4, 4, 'Organic', 21, 'Marans', '2775 el capitan drive', 'colorado springs', 'co', 80918, NULL, 38.908058, -104.775224, 0),
+(5, 9, 'Organic', 56, 'Silkie', '3705 windsor avenue', 'colorado springs', 'co', 80907, NULL, 38.88452, -104.794899, 0);
 
 -- --------------------------------------------------------
 
@@ -81,17 +113,21 @@ CREATE TABLE IF NOT EXISTS `Users` (
   `fname` varchar(128) NOT NULL,
   `lname` varchar(128) NOT NULL,
   `dname` varchar(128) NOT NULL,
+  `pass` varchar(10) NOT NULL,
   `email` varchar(256) NOT NULL,
   `phone` int(10) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `Users`
 --
 
-INSERT INTO `Users` (`userID`, `sellerID`, `fname`, `lname`, `dname`, `email`, `phone`) VALUES
-(1, NULL, 'Ben', 'Matson', 'DragonMaster', 'master6@bogus.com', 1234567890),
-(2, NULL, 'Janet', 'Atkins', 'jatkins', 'jatkins@bogus.com', 2147483647);
+INSERT INTO `Users` (`userID`, `sellerID`, `fname`, `lname`, `dname`, `pass`, `email`, `phone`) VALUES
+(1, 1, 'Ben', 'Matson', 'DragonMaster', 'dragon', 'master6@pikesbeak.com', 1234567890),
+(2, 2, 'Janet', 'Atkins', 'jatkins', 'pass', 'jatkins@pikesbeak.com', 2147483647),
+(3, 4, 'Daniel', 'Taylor', 'dtaylor', 'pass', 'dtaylor@pikesbeak.com', 1472583696),
+(4, 3, 'Patrick', 'Anderson', 'panderson', 'pass', 'panderson@pikesbeak.com', 147820369),
+(5, 5, 'Kendra', 'Wilson', 'kwilson', 'pass', 'kwilson@pikesbeak.com', 1236547890);
 
 -- --------------------------------------------------------
 
@@ -116,7 +152,7 @@ CREATE TABLE IF NOT EXISTS `WaitLists` (
 --
 ALTER TABLE `Listings`
   ADD PRIMARY KEY (`listID`),
-  ADD UNIQUE KEY `sellerID` (`sellerID`);
+  ADD UNIQUE KEY `listID` (`listID`);
 
 --
 -- Indexes for table `Notifications`
@@ -134,7 +170,9 @@ ALTER TABLE `Sellers`
 -- Indexes for table `Users`
 --
 ALTER TABLE `Users`
-  ADD PRIMARY KEY (`userID`);
+  ADD PRIMARY KEY (`userID`),
+  ADD UNIQUE KEY `userID` (`userID`),
+  ADD UNIQUE KEY `dname` (`dname`);
 
 --
 -- Indexes for table `WaitLists`
@@ -150,7 +188,7 @@ ALTER TABLE `WaitLists`
 -- AUTO_INCREMENT for table `Listings`
 --
 ALTER TABLE `Listings`
-  MODIFY `listID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `listID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=13;
 --
 -- AUTO_INCREMENT for table `Notifications`
 --
@@ -160,12 +198,12 @@ ALTER TABLE `Notifications`
 -- AUTO_INCREMENT for table `Sellers`
 --
 ALTER TABLE `Sellers`
-  MODIFY `sellerID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `sellerID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT for table `Users`
 --
 ALTER TABLE `Users`
-  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT for table `WaitLists`
 --
