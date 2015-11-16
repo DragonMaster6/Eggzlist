0fec35b60e5b254291d5f04b6d29c41fce2945c0
Patrick Anderson <rcaviator2992@gmail.com>
2015-11-16 13:18:07 -0600

    created about us page

---
+++ application/views/users/about.php
@@ -0,0 +1,37 @@
+<? php
+/*
+	This is the About Us page. All relevent Eggzlist company information
+	that the founders wish to display to the end users will be on this page.
+*/
+?>
+
+<center>
+	<div id="about_container">
+		<div id="about_header">
+			<h2>About Us</h2>
+		</div>
+		
+		<div id="about_desc">
+			<!-- Content of the page -->
+			<ol>
+				<!-- Information about the company story and founder profiles -->
+				<li>Company story and profiles</li>
+					<ul>
+						<li>Company story</li>
+						<li>Client 1</li>
+						<li>Client 2</li>
+						<li>Client 3</li>
+						<li>Client 4</li>
+					</ul>
+				<!-- Information about the client contact information -->
+				<li>Contact information</li>
+					<ul>
+						<li>Client 1</li>
+						<li>Client 2</li>
+						<li>Client 3</li>
+						<li>Client 4</li>
+					</ul>
+			</ol>
+		</div>
+	</div>
+</center>
