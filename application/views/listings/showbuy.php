7cd8ec25509fe6ad5fe18b8ec848c2e8021dd9e4
DanSyron <Syron@Dannys-MacBook-Air.local>
2015-11-23 11:16:36 -0700

    dan made changes to the showbuy page

--- application/views/listings/showbuy.php
+++ application/views/listings/showbuy.php
@@ -1,4 +1,6 @@
+
 <center>
+
 
 <div id = "seller_map">
 
@@ -8,42 +10,42 @@
 <table>
 <tr>
 	<th colspan = "3">
-		<?php echo $listing['title'] ?>
-	</th>
-</tr>
 
-<tr>
-	<td>
-		Delivery Type: 
+		<?php echo $listing['title']?> - <?php echo "$".$listing['price'] ?> (crossroad goes here)
+<br>
+<br>
+
+
+<button id = "contact_btn">Contact Seller </button>
+<br>
+<br>
+
+
+			Egg Inventory:
+		<?php echo $listing['inventory']." eggs"; ?>
+		<br>
+				Delivery Type: 
 		<?php 
 			if($listing['pickup'] == 0){
 				echo "Drop off";
 			}else{
 				echo "Pick up";
 			}
-		?>
-	</td>
+			?>
 
-	<td>
-		Price:
-		<?php echo "$".$listing['price'] ?>
-	</td>
-
-	<td>
-		Egg Inventory:
-		<?php echo $listing['inventory']." eggs"; ?>
-
-	</td>
+			
 
 
-
-
-
-
+	</th>
 </tr>
+
+
+
+
 
 <tr>
 	<td colspan = 3>
+		<br>
 		<?php echo $listing['description']; ?>
 My eggs are the best eggs in town!  Lorem Ipsum nonsense yada yada stuff here!  Come buy my stuff.
 My eggs are the best eggs in town!  Lorem Ipsum nonsense yada yada stuff here!  Come buy my stuff.
