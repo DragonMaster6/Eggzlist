52bceb361a07c086646760dd95b5de4d95530af5
Ben Matson <bmatson@uccs.edu>
2015-12-06 19:08:46 -0700

    Last push for presentation: Updated DB user accounts

--- Eggzlist_db.sql
+++ Eggzlist_db.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Dec 03, 2015 at 04:04 AM
+-- Generation Time: Dec 07, 2015 at 03:07 AM
 -- Server version: 5.6.26
 -- PHP Version: 5.6.12
 
@@ -39,7 +39,7 @@ CREATE TABLE IF NOT EXISTS `Listings` (
   `rating` int(10) unsigned NOT NULL,
   `inventory` int(11) NOT NULL,
   `private` tinyint(1) NOT NULL DEFAULT '0'
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `Listings`
@@ -47,8 +47,9 @@ CREATE TABLE IF NOT EXISTS `Listings` (
 
 INSERT INTO `Listings` (`listID`, `sellerID`, `title`, `description`, `pickup`, `price`, `status`, `start`, `finish`, `rating`, `inventory`, `private`) VALUES
 (3, 15, 'Fresh Eggs from the Springs', 'Hello one and all! I have some fresh eggs here for our Omelette wednesdays so come one, come all!<br>\r\n\r\nEdit: We are running out! Come quick!\r\n<br>\r\n<br>\r\nWe have more now', 1, 5, 0, '2015-11-30 20:12:47', NULL, 0, 39, 0),
-(4, 16, 'Fresh eggz as of 12/1', 'Our chickens have just laid so come get them while they are fresh! Forget Omelette Wednesdays come to Egg Tuesdays!', 1, 4, 0, '2015-12-01 09:57:12', NULL, 0, 24, 0),
-(5, 17, 'Eggs for sale', 'These are magic eggs... :)', 1, 4, 0, '2015-12-01 17:20:40', NULL, 0, 35, 0);
+(5, 17, 'Eggs for sale', 'These are magic eggs... :)', 1, 4, 0, '2015-12-01 17:20:40', NULL, 0, 35, 0),
+(7, 16, 'Great Eggz for sale', 'My eggz are delicious', 1, 5, 0, '2015-12-05 13:31:10', NULL, 0, 12, 0),
+(9, 20, 'Fresh Eggs from DanTheMan', 'I have fresh eggs every week so come buy my eggs!', 1, 5, 0, '2015-12-05 16:36:39', NULL, 0, 24, 0);
 
 -- --------------------------------------------------------
 
@@ -65,14 +66,16 @@ CREATE TABLE IF NOT EXISTS `Notifications` (
   `message` varchar(1024) NOT NULL,
   `posted` datetime NOT NULL,
   `eggs` int(12) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `Notifications`
 --
 
 INSERT INTO `Notifications` (`noteID`, `toUserID`, `fromUserID`, `type`, `subject`, `message`, `posted`, `eggs`) VALUES
-(2, 20, 21, 2, 'MAGIC EGGS', 'I WANT YOUR MAGIC EGGS!', '2015-12-01 17:31:08', 12);
+(2, 20, 21, 2, 'MAGIC EGGS', 'I WANT YOUR MAGIC EGGS!', '2015-12-01 17:31:08', 12),
+(4, 19, 16, 2, 'I want you eggz', 'PLEAZE! I want your eggs', '2015-12-05 13:32:59', 5),
+(5, 24, 25, 2, 'I want your Eggz!', 'I need Eggs! Let me take some off your hands!', '2015-12-05 16:41:01', 12);
 
 -- --------------------------------------------------------
 
@@ -94,7 +97,7 @@ CREATE TABLE IF NOT EXISTS `Sellers` (
   `lat` double NOT NULL,
   `lng` double NOT NULL,
   `rating` int(11) NOT NULL DEFAULT '0'
-) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `Sellers`
@@ -105,7 +108,8 @@ INSERT INTO `Sellers` (`sellerID`, `numChick`, `feed`, `eggrate`, `breeds`, `str
 (16, 5, 'pikes beak', 28, 'marans,leg horn', '104th Drive', 'westminster', 'co', 80021, '100th Ave and Wadsworth Parkway', 39.920491, -105.087725, 0),
 (17, 6, 'pikes beak', 35, 'silky', 'uccs', 'colorado springs', 'co', 80919, 'austin bluffs', 38.833358, -104.820851, 0),
 (18, 8, 'ASLDFJKASDF', 24, 'ASL;FDJAS;DKFLJ;', 'KINGS ROAD', 'ADA', 'OK', 74820, 'KINGS ROAD AND BROADWAY', 34.771926, -96.680535, 0),
-(19, 2, 'pikes beak', 14, 'other', 'streetName', 'CS', 'CO', 80000, 'asdfghj ggjygc hhyf', 38.833358, -104.820851, 0);
+(19, 2, 'pikes beak', 14, 'other', 'streetName', 'CS', 'CO', 80000, 'asdfghj ggjygc hhyf', 38.833358, -104.820851, 0),
+(20, 6, 'pikes beak', 42, 'silkie,marans', 'Mallow Rd.', 'colorado springs', 'co', 80918, 'Mallow Rd and Cragmor Rd', 38.89302, -104.812862, 0);
 
 -- --------------------------------------------------------
 
@@ -122,20 +126,22 @@ CREATE TABLE IF NOT EXISTS `Users` (
   `pass` varchar(10) NOT NULL,
   `email` varchar(256) NOT NULL,
   `phone` varchar(10) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `Users`
 --
 
 INSERT INTO `Users` (`userID`, `sellerID`, `fname`, `lname`, `dname`, `pass`, `email`, `phone`) VALUES
-(16, NULL, 'JANET', 'ATKINS', 'JATKINS', 'iyaayas.', 'janet_atkins11@hotmail.com', '2108420138'),
+(16, NULL, 'JANET', 'ATKINS', 'JATKINS', 'pass', 'janet_atkins11@hotmail.com', '2108420138'),
 (18, 15, 'RICH', 'ATKINS', 'RATKINS', 'iyaayas.', 'janet_atkins11@hotmail.com', '2108420138'),
 (19, 16, 'Ben', 'Matson', 'TheEggMaster', 'dragon', 'dragonmaster1694@gmail.com', '3036192400'),
 (20, 17, 'karlie', 'van arnam', 'karlie', 'password', 'fake@gmail.com', '7196600718'),
 (21, NULL, 'ROB', 'BRUNK', 'ROB', 'PASSWORD', 'ASFDASDF', '469.525.12'),
 (22, 18, 'R', 'BRUNK', 'ROBSTER', 'PASSWORD', 'ERADSFASFD', '235423423'),
-(23, 19, 'firstName', 'lastName', 'asdf', 'pass', 'fake@fake.com', '0123456789');
+(23, 19, 'firstName', 'lastName', 'asdf', 'pass', 'fake@fake.com', '0123456789'),
+(24, 20, 'Daniel', 'Taylor', 'DanTheMan', 'pass', 'dtaylor@pikebeak.com', '(720) 123 '),
+(25, NULL, 'Patrick', 'Anderson', 'EggBuyer3000', 'pass', 'panderson@pikesbeak.com', '(719) 222 ');
 
 -- --------------------------------------------------------
 
@@ -149,7 +155,7 @@ CREATE TABLE IF NOT EXISTS `WaitLists` (
   `userID` int(11) NOT NULL,
   `start` datetime NOT NULL,
   `finish` datetime DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `WaitLists`
@@ -157,7 +163,10 @@ CREATE TABLE IF NOT EXISTS `WaitLists` (
 
 INSERT INTO `WaitLists` (`waitID`, `listID`, `userID`, `start`, `finish`) VALUES
 (1, 3, 20, '2015-12-01 17:16:45', NULL),
-(2, 5, 21, '2015-12-01 17:31:08', NULL);
+(2, 5, 21, '2015-12-01 17:31:08', NULL),
+(3, 6, 16, '2015-12-05 12:06:43', NULL),
+(4, 7, 16, '2015-12-05 13:32:59', NULL),
+(5, 9, 25, '2015-12-05 16:41:01', NULL);
 
 --
 -- Indexes for dumped tables
@@ -204,27 +213,27 @@ ALTER TABLE `WaitLists`
 -- AUTO_INCREMENT for table `Listings`
 --
 ALTER TABLE `Listings`
-  MODIFY `listID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
+  MODIFY `listID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=10;
 --
 -- AUTO_INCREMENT for table `Notifications`
 --
 ALTER TABLE `Notifications`
-  MODIFY `noteID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+  MODIFY `noteID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT for table `Sellers`
 --
 ALTER TABLE `Sellers`
-  MODIFY `sellerID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=20;
+  MODIFY `sellerID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=21;
 --
 -- AUTO_INCREMENT for table `Users`
 --
 ALTER TABLE `Users`
-  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=24;
+  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=26;
 --
 -- AUTO_INCREMENT for table `WaitLists`
 --
 ALTER TABLE `WaitLists`
-  MODIFY `waitID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+  MODIFY `waitID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
